5ef07e26b98c81ede0107311440ce17ab07aca41
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-11-02 12:31:18 +0530

    Update CheckingPrimeOrNot.cpp

--- Mathematics/CheckingPrimeOrNot.cpp
+++ Mathematics/CheckingPrimeOrNot.cpp
@@ -31,7 +31,7 @@ int checkPrime(int n)
    else if(n==2 || n==3) return true;
    else if(n%2 == 0 || n%3 == 0) return false;
    else{
-   for(int i=5;i*i<n;i++)
+   for(int i=5;i*i<n;i+6)
    {
      if(n%i == 0)
       return false;
