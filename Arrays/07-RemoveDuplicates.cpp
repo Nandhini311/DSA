647dd33bce524b5941bcd87585797de46ffd46d6
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-09 18:48:57 +0530

    Create 07-RemoveDuplicates.cpp

---
+++ Arrays/07-RemoveDuplicates.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int RemoveDuplicates(int arr[])
+{
+    int temp[n];
+    temp[0]=arr[0];                           //copying the first element to the temporary array
+    int res=1;                                //position of temp
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i-1]!=arr[i])                  //checking if any two consequtive number is same
+        {
+            temp[res]=arr[i];
+            res++;
+        }
+    }
+    return temp;
+}
