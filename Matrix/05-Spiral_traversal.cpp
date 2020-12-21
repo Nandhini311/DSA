b95b6205cb04eec1a7ae33017a7c3754abdb7063
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-21 19:54:47 +0530

    Create 05-Spiral_traversal.cpp

---
+++ Matrix/05-Spiral_traversal.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <bits/stdc++.h> 
+using namespace std;
+
+const int R = 4, C = 4;
+
+void printSpiral(int mat[4][4], int R, int C)
+{
+	int top = 0, left = 0, bottom = R - 1, right = C - 1;
+
+	while(top <= bottom && left <= right)
+	{
+		for(int i = left; i <= right; i++)  //printing top row
+			cout << mat[top][i] << " ";
+
+		top++;
+
+		for(int i = top; i <= bottom; i++)  //right column
+			cout << mat[i][right] << " " ;
+		
+		right--;
+
+		if(top <= bottom){
+		for(int i = right; i >= left; i--)  //bottom reverse
+			cout << mat[bottom][i] << " ";
+
+		bottom--;
+		}
+
+		if(left <= right){
+		for(int i = bottom; i >= top; i--)  //left reverse
+			cout << mat[i][left] << " ";
+
+		left++;
+		}			
+	}
+}
+
+int main()
+{	
+	int arr[R][C] = {{1, 2, 3, 4},
+				    {5, 6, 7, 8},
+				    {9, 10, 11, 12},
+				    {13, 14, 15, 16}};
+
+  printSpiral(arr, R, C);
+
+	return 0;
+}
