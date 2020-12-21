cf782396c9d73da56202f63ccdd71be0b5fe8e81
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-21 19:57:50 +0530

    Create 01-BinarySearch.cpp

---
+++ Searching/01-BinarySearch.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+//iterative
+int bSearch(int arr[], int n, int x)
+{
+	int low = 0, high = n - 1;
+
+	while(low <= high)
+	{
+		int mid = (low + high) / 2;
+
+		if(arr[mid] == x)
+			return mid;
+
+		else if(arr[mid] > x)
+			high = mid - 1;
+
+		else
+			low = mid + 1;
+	}
+
+	return -1;
+}
+
+//recurssive
+int bSearch(int arr[], int low, int high, int x)
+{
+	if(low > high)
+		return -1;
+
+	int mid = (low + high) / 2;
+
+	if(arr[mid] == x)
+		return mid;
+
+	else if(arr[mid] > x)
+		return bSearch(arr, low, mid - 1, x);
+
+	else
+		return bSearch(arr, mid + 1, high, x);
+}
+
+int main() {
+    
+    int arr[] = {10, 20, 30, 40, 50, 60}, n = 6;
+
+	int x = 25;
+	
+	cout<<bSearch(arr, n, x);
+	return 0;
+}
