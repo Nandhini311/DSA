9237118fd589772397df9c9b8d56519729714ed8
Medusa <63045112+Nandhini311@users.noreply.github.com>
2020-12-28 23:58:50 +0530

    Create 05-Count-One's.cpp

---
+++ Searching/05-Count-One's.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int CountOne(int arr[], int n)
+{
+   int low = 0, high = n-1;
+   while(low<high)
+   {
+     int mid = (low+high)/2;
+     if(arr[mid]==0)
+        low=mid+1;
+     else
+       {
+        if(mid==0||arr[mid-1]==0)
+           return n-mid;
+        else 
+           high = mid+1;
+        }
+   }
+   return -1;
+}
+
+int main() {
+    
+   int arr[] = {0, 0, 1, 1, 1, 1}, n = 6;
+
+   cout << countOnes(arr, n);
+
+	return 0;
+}
